deec435177214cf5bb818b73617555578984b3d8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-22 22:48:54 -0600

    --Starting: Added starter code for API Response with Swift Data

--- CachingAPIResponseSwiftDataPracticeTD/ContentView.swift
+++ CachingAPIResponseSwiftDataPracticeTD/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { _ in
+                    VStack(alignment: .leading) {
+                        Rectangle()
+                            .fill(.blue)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 300)
+                            
+                        Text("[Title here]")
+                            .font(.caption)
+                            .bold()
+                            .padding(.horizontal)
+                            .padding(.top)
+                    }
+                    
+                    .padding(.bottom)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10,
+                                                style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                    
+                   
+                }
+                .listRowSeparator(.hidden)
+                .listRowBackground(Color.clear)
+            }
+            .navigationTitle("Posts")
+            .listStyle(.plain)
+            .scrollContentBackground(.hidden)
+            .background(Color(uiColor: .systemGroupedBackground))
         }
-        .padding()
     }
 }
 
