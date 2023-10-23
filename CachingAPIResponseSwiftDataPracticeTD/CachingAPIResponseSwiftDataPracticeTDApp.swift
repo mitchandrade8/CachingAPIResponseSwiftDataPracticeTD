d183b46d9267e4e1d339a59af491a5de318452e8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-22 22:46:54 -0600

    Initial Commit

---
+++ CachingAPIResponseSwiftDataPracticeTD/CachingAPIResponseSwiftDataPracticeTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachingAPIResponseSwiftDataPracticeTDApp.swift
+//  CachingAPIResponseSwiftDataPracticeTD
+//
+//  Created by Mitch Andrade on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CachingAPIResponseSwiftDataPracticeTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
